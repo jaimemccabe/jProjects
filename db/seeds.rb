02b300bed5a0dd6de788841534d4a37ccb28afeb
Jaime McCabe <mccabejaime@gmail.com>
2012-03-24 19:09:46 -0500

    made a seeds file to create dummy data, ALSO, made the data show up in the project show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.destroy_all
+  Story.destroy_all
+   Task.destroy_all
+
+Project.create name: "Jaime's Project"
+Project.create name: "Kori's Project"
+
+Story.create name: "Read ruby", project_id: Project.first.id
+Story.create name: "Learn Rails", project_id: Project.last.id
+
+story_count = Story.count
+first = Story.first.id
+last = Story.last.id
+
+10.times do |t|
+  Task.create title: "Task#{t}", story_id: rand(first..last), status: rand(1..4)
+end
