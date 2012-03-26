46ce3d599eed084063e314c2bd2e1eae2b89bc73
Kori Roys <kori@koriroys.com>
2012-03-26 11:16:36 -0500

    used group by to organize the project show page a little better

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,16 @@
 module StoriesHelper
+  
+  def status_column_text(status)
+    case status
+    when 1
+      "Not Yet Started"
+    when 2
+      "In Progress"
+    when 3
+      "In Review"
+    when 4
+      "Complete"
+    end
+  end
+  
 end
