2a9a20228f5deded58cf8d1773a32e27a95fe6d3
Kori Roys <kori@koriroys.com>
2012-04-08 17:40:04 -0500

    moved add and remove project/story buttons around

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -77,7 +77,7 @@ class ProjectsController < ApplicationController
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to root_path }
       format.json { head :no_content }
     end
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,5 @@ class Project < ActiveRecord::Base
   has_many :stories, :dependent => :destroy
   has_many :tasks, :through => :stories
   
+  validates :name, :presence => true
 end
